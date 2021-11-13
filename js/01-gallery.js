aa932d2dc261c91b73801a36c4649129180a1f57
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-13 23:24:44 +0200

    adds 1st Task not solved

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryPic = galleryItems.map((galleryItem) =>
+`<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryPic);
+
+
+const openOriginal = function (event) {
+    
+    const instance = basicLightbox.create(`<img scr="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600"/>`);
+    instance.show();
+    
+    event.preventDefault();
+};
+
+gallery.addEventListener("click", openOriginal);
