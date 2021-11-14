55990f87e69d73f6558aba4acff9df5e822ce56b
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-14 16:14:50 +0200

    add some fixes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,41 +4,65 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(".gallery");
 
-const galleryPic = galleryItems.map((galleryItem) =>
+const galleryPic = galleryItems.map((galleryItem, index) =>
 `<div class="gallery__item">
   <a class="gallery__link" href="${galleryItem.original}">
     <img
       class="gallery__image"
       src="${galleryItem.preview}"
       data-source="${galleryItem.original}"
+      data-index="${index}"
       alt="${galleryItem.description}"
     />
   </a>
 </div>`).join("");
 
+let currentIndex = 0;
+
 gallery.insertAdjacentHTML("afterbegin", galleryPic);
 
+const instance = basicLightbox.create(`<img src="" alt="" width="800" height="600"/>`, {onShow: (instance) => {window.addEventListener('keydown', onEscClose)}, onClose: (instance) => {window.removeEventListener('keydown', onEscClose)}});
 
-const openOriginal = function (event) {
-  
-    const instance = basicLightbox.create(`<img srс="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600"/>`);
-    instance.show();
-    
-  event.preventDefault();
-  
-  
-  const onEscClose = function (event) {
-    if (event.code === "Escape") {
-      instance.close();
-      window.removeEventListener('keydown', onEscClose);
-    };
-    
-  };
-  window.addEventListener('keydown', onEscClose);
-  
+function setModalSrc(url) {
+  instance.element().querySelector('img').src = url;
 };
 
+const onEscClose = function (event) {
+  if (event.code === "Escape") {
+      instance.close();
+      
+  };
+  if (event.code === 'ArrowLeft') {
+    
 
+    currentIndex -= 1;
+    if (currentIndex <= 0) {
+      currentIndex = galleryItems.length - 1;
+    }
+    setModalSrc(galleryItems[currentIndex].original);
+  };
+  if (event.code === 'ArrowRight') {
+    
+
+    currentIndex += 1;
+    if (currentIndex >= galleryItems.length - 1) {
+      currentIndex = 0;
+    }
+    setModalSrc(galleryItems[currentIndex].original);
+  }
+};
+const openOriginal = function (event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+  
+  currentIndex = Number(event.target.dataset.index);
+  console.log(currentIndex);
+  
+  setModalSrc(event.target.dataset.source);
+    instance.show();
+     
+  };
+  
 gallery.addEventListener("click", openOriginal);
 
 
