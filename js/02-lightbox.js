d0e15a6eb38565fefc72086e3e88c1371de874b9
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-14 13:47:12 +0200

    adds Task-2 almost done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryPic = galleryItems.map((galleryItem) =>
+`<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryPic);
+
+const openOriginal = function (event) {
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'event.target.alt', captionPosition: 'bottom', captionDelay: 250});
+    
+    event.preventDefault();
+};
+
+gallery.addEventListener('click', openOriginal);
+
+
